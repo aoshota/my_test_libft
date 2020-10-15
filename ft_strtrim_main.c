3b7558fc1b0249d4e712d28f61997137bc58e447
Shota Aoyama <syouta.aoyama20@gmail.com>
2020-10-15 19:02:31 +0900

    a

--- ft_strtrim_main.c
+++ ft_strtrim_main.c
@@ -16,19 +16,19 @@
 
 int	main(int argc, char **argv)
 {
-	char *p;
-
-	if (argc < 4)
+	if (argc < 3)
 	{
 		printf("\n --- input like this ---\n");
-		printf(" --- ./a.out \"Hello world!\" \"7\" \"5\" ---\n\n");
+		printf(" --- ./a.out \"--++--string--+--\" \"-+\" ---\n\n");
 	}
 	else
 	{
-		printf("\n s = %s\n", argv[1]);
-		printf(" p = ft_substr(%s, %s, %s)\n", argv[1], argv[2], argv[3]);
-		p = ft_substr(argv[1], atoi(argv[2]), atoi(argv[3]));
-		printf(" p = %s\n", p);
+		printf("\n s1 = %s\n", argv[1]);
+		printf(" set = %s\n", argv[2]);
+		printf(" p = ft_strtrim(s1, set)\n");
+		char	*p;
+		p = ft_strtrim(argv[1], argv[2]);
+		printf(" p = %s\n\n", p);
 	}
 	return(0);
 }
