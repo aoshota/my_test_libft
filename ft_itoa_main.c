9448ab79e75b54053016eefa6ebefa8b3c3d7788
aoyama <syouta.aoyama20@gmail.com>
2020-10-17 11:46:39 +0900

    add ft_itoa_main.c

---
+++ ft_itoa_main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy_main.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saoyama <saoyama@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/15 13:01:44 by saoyama           #+#    #+#             */
+/*   Updated: 2020/10/15 13:01:44 by saoyama          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+#include "libft.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		printf("\n --- input number like this ---\n");
+		printf(" --- ./a.out \"number\"       ---\n\n");
+	}
+	else
+	{
+		printf("\n %s\n\n", ft_itoa(atoi(argv[1])));
+	}
+	return(0);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: saoyama <saoyama@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/09 02:16:39 by saoyama           #+#    #+#             */
-/*   Updated: 2020/10/13 14:51:50 by saoyama          ###   ########.fr       */
+/*   Updated: 2020/10/15 20:23:53 by saoyama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define LIBFT_H
 
 # include <stdlib.h>
+# include <unistd.h>
 
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
@@ -39,5 +40,15 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	*ft_itoa(int n);
+char	**ft_split(char const *s, char *c);
 
 #endif
