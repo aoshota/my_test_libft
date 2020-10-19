3af4e20edc795849a345a91cab742cbcea6c234a
aoyama <syouta.aoyama20@gmail.com>
2020-10-20 00:37:52 +0900

    a

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: saoyama <saoyama@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/09 02:16:39 by saoyama           #+#    #+#             */
-/*   Updated: 2020/10/18 14:04:48 by saoyama          ###   ########.fr       */
+/*   Updated: 2020/10/19 22:24:46 by saoyama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,8 @@ size_t				ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t				ft_strlcat(char *dst, const char *src, size_t dstsize);
 char				*ft_strchr(const char *s, int c);
 char				*ft_strrchr(const char *s, int c);
-char				*ft_strnstr(const char *s, const char *needle, size_t len);
+char				*ft_strnstr(const char *haystack, \
+								const char *needle, size_t len);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 int					ft_atoi(const char *str);
 int					ft_isalpha(int c);
