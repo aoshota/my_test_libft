243d10f08b5ef63e88bc4d4943f831f0d2b2153f
aoyama <syouta.aoyama20@gmail.com>
2020-10-20 01:00:03 +0900

    a

--- test.c
+++ test.c
@@ -11,4 +11,14 @@ int main()
 
 	printf("\n INT_MAX = %d\n", INT_MAX);
 	printf(" INT_MAX + 1 = %d\n\n", (int)x);
+
+  char *p1 = "\0";
+  char *p2;
+
+  p2 = NULL;
+
+  if (!*p1)
+    printf("\n p1 = %p\n", p1);
+  if (!p2)
+    printf(" p2 = %p\n\n", p2);
 }
